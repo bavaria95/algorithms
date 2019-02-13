6d0b79a0fce7078c5df89036cc426fc47c48d82d
Dmytro Petruk <bavaria95@gmail.com>
2019-02-13 23:28:20 +0100

    Insertion method for BST

--- algorithms/trees/bst.py
+++ algorithms/trees/bst.py
@@ -12,3 +12,35 @@ class TreeNode(object):
 
     def __lt__(self, other):
         return self.key < other.key
+
+
+class BSTree(object):
+    def __init__(self):
+        self.root = None
+
+    def _insert(self, root, node):
+        if node < root:
+            # insert to the left subtree
+            if not root.left:
+                # if no left subtree - append there
+                root.left = node
+                return
+            # otherwise keep going till reach node with no left leaf
+            self._insert(root.left, node)
+        else:
+            # insert to the right subtree
+            if not root.right:
+                root.right = node
+                return
+            self._insert(root.right, node)
+
+
+    def insert(self, node):
+        if self.root is None:
+            self.root = node
+        else:
+            self._insert(self.root, node)
+
+
+if __name__ == '__main__':
+    pass
