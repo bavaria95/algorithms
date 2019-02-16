356d98ebc35c3887213bb7ba920845f99f49eb86
Dmytro Petruk <bavaria95@gmail.com>
2019-02-16 13:00:09 +0100

    Node removal

--- algorithms/trees/bst.py
+++ algorithms/trees/bst.py
@@ -63,6 +63,101 @@ class BSTree(object):
         else:
             self._insert(self.root, node)
 
+    def _remove_child(self, node):
+        parent = node.parent
+        if not parent:
+            self.root = None
+            return
+
+        if parent.left is node:
+            parent.left = None
+
+        if parent.right is node:
+            parent.right = None
+
+    def remove_by_node(self, node):
+        if not node:
+            return
+
+        if not node.left and not node.right:
+            # no children - just remove it
+            self._remove_child(node)
+            del node
+            return
+
+        if bool(node.left) ^ bool(node.right):
+            # if node has only one child
+
+            child = node.left if node.left else node.right
+
+            parent = node.parent
+            if not parent:
+                self.root = child
+
+            if parent and parent.left is node:
+                parent.left = child
+            elif parent:
+                parent.right = child
+
+            child.parent = parent
+            return
+
+        # if both children are present
+        # finding successor in-order node
+        succ = self.min(node.right)
+
+        # swapping it with node to remove
+        self._swap_nodes(node, succ)
+
+        if not succ.parent:
+            self.root = succ
+
+        self.remove_by_node(node)
+        del node
+
+    def remove(self, key):
+        node = self.search(key)
+        self.remove_by_node(node)
+
+    def _swap_nodes(self, node1, node2):
+        if node1.left:
+            node1.left.parent = node2
+        if node1.right:
+            node1.right.parent = node2
+
+        if node2.left:
+            node2.left.parent = node1
+        if node2.right:
+            node2.right.parent = node1
+
+        if node1.parent and node1.parent.left and node1.parent.left is node1:
+            node1.parent.left = node2
+        if node1.parent and node1.parent.right and node1.parent.right is node1:
+            node1.parent.right = node2
+        if node2.parent and node2.parent.left and node2.parent.left is node2:
+            node2.parent.left = node1
+        if node2.parent and node2.parent.right and node2.parent.right is node2:
+            node2.parent.right = node1
+
+
+        node1.parent, node2.parent = node2.parent, node1.parent
+        node1.left, node2.left = node2.left, node1.left
+        node1.right, node2.right = node2.right, node1.right
+
+
+    def min(self, node=None):
+        # find the smaller key starting from the node
+        # if node is not passed - finds minumum in the whole tree
+        if not node:
+            it = self.root
+        else:
+            it = node
+
+        while it.left:
+            it = it.left
+
+        return it
+
 
 if __name__ == '__main__':
     import random
@@ -83,3 +178,10 @@ if __name__ == '__main__':
         assert tree.root is tree.root.left.parent
     else:
         assert tree.root is tree.root.right.parent
+
+    for i in range(20):
+        assert bool(tree.search(i))
+
+    for i in range(20):
+        tree.remove(i)
+        assert not bool(tree.search(i))
