71e0085249323663d7c3b71e6aa79de93abb7e44
Dmytro Petruk <bavaria95@gmail.com>
2019-02-17 18:49:29 +0100

    Leetcode 94

---
+++ leetcode/p94.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def dfs(self, node, a):
+        if not node:
+            return
+        
+        self.dfs(node.left, a)
+        a.append(node.val)
+        self.dfs(node.right, a)
+        
+    def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        a = []
+        self.dfs(root, a)
+        return a
