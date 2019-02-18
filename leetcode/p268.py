3ab82a24322ab103d8f70da35dcc170d8299dcb9
Dmytro Petruk <bavaria95@gmail.com>
2019-02-18 21:32:47 +0100

    Leetcode 268

---
+++ leetcode/p268.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        return int(0.5*(n * (n + 1))) - sum(nums)
